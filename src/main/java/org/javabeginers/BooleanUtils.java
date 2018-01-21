abdd7bbea073b71c068b87975020790ee615eadc
ТоварищКотейка <zolander777@mail.ru>
2018-01-21 13:35:35 +0700

    Ну как то так. Параметр,что идет в метод я пока оставил String

--- src/main/java/org/javabeginers/BooleanUtils.java
+++ src/main/java/org/javabeginers/BooleanUtils.java
@@ -1,8 +1,15 @@
-package org.javabeginers;
+package main.java.org.javabeginers;
 
 /**
  * Класс утилит для работы с логическим типом данных.
  */
 public class BooleanUtils {
+    public static boolean convertToBoolean(String value) {
+        boolean returnValue = false;
+        if ("1".equalsIgnoreCase(value) || "yes".equalsIgnoreCase(value) ||
+                "true".equalsIgnoreCase(value) || "on".equalsIgnoreCase(value))
+            returnValue = true;
+        return returnValue;
+    }
 
 }
--- src/test/java/org/javabeginers/BooleanUtilsTest.java
+++ src/test/java/org/javabeginers/BooleanUtilsTest.java
@@ -1,4 +1,7 @@
-package org.javabeginers;
+package test.java.org.javabeginers;
+
+import main.java.org.javabeginers.BooleanUtils;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -6,5 +9,13 @@ import static org.junit.Assert.*;
  * Тестовый класс для проверки BooleanUtils
  */
 public class BooleanUtilsTest {
+
+    @Test
+    public void testConvertToBoolean() {
+        assertTrue(BooleanUtils.convertToBoolean("1"));
+        assertFalse(BooleanUtils.convertToBoolean("0"));
+        assertFalse(BooleanUtils.convertToBoolean("False"));
+        assertFalse(BooleanUtils.convertToBoolean("bla bla"));
+    }
 
 }
